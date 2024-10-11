9046243fc8f6ae5637382a1678811cd6fbb8dab4
yousef266 <ym190836@gmail.com>
2024-10-11 11:47:45 +0300

    Initial commit

---
+++ lib/views/category_view.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:news_app_ui_setup/widgets/categories_list_view.dart';
+import '../widgets/news_list_view_builder.dart';
+
+class CategoryView extends StatelessWidget {
+  final String category;
+
+  const CategoryView({super.key, required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: const Color.fromARGB(0, 144, 199, 16),
+        title: Text(
+          '$category News',
+          style: const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: CustomScrollView(
+          physics: const BouncingScrollPhysics(),
+          slivers: [
+            const SliverToBoxAdapter(
+              child: CategoriesListView(), // Keeps the categories list view at the top
+            ),
+            const SliverToBoxAdapter(
+              child: SizedBox(
+                height: 32,
+              ),
+            ),
+            NewsListViewBuilder(
+              category: category,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
